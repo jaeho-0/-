e8866ef9c239f0e6b9b77adb2e0ff2c49d29b889
jaeho-0 <ser0714@naver.com>
2025-07-09 14:19:28 +0900

    Add files via upload

---
+++ 사용설명서.py
@@ -0,0 +1,6 @@
+#API_KEY = os.getenv('GOOGLE_API_KEY')
+#구글 api는 https://console.cloud.google.com/ 접속 후 custom search api 등록하여 api 생성 후 복사
+
+#CSE_ID = os.getenv('GOOGLE_CSE_ID')
+#https://programmablesearchengine.google.com/ 접속 후 검색 엔진 프로그래밍 후 복사
+#.env 파일 생성 후 사용하세요.
